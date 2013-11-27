fab86249af89055411d260c1058689f80c7ddf5d
Mark Gardner <gardnerm@familysearch.org>
2013-11-27 08:48:02 -0700

    Adding example

---
+++ example/simple/app.js
@@ -0,0 +1,59 @@
+var githoist = require('../../lib/index'),
+	path = require('path'),
+	fs = require('fs'),
+	homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+
+githoist.start({
+	config: {
+		http: {
+			sitename: 'Git-Hoist Sample',
+			hostname: 'localhost',
+			port: 9080/*,
+			cert: {
+				key: fs.readFileSync(path.join(__dirname, 'host.key')),
+				cert: fs.readFileSync(path.join(__dirname, 'server.crt'))
+			}*/
+		},
+		ssh: {
+			auth_keys: path.join(homeDir, '.ssh/authorized_keys')
+		},
+		repo: {
+			default_access: {
+				'@admins': githoist.access.READ + githoist.access.WRITE,
+				'@limited': githoist.access.READ
+			},
+			directory: path.join(homeDir, '.githoist/repos')
+		}
+	},
+	users: new githoist.auth.Basic({
+		groups: ['admins', 'limited'],
+		users: {
+			kevin_smiles: {
+				password: 'test',
+				sshKey: '<* you pub ssh key *>',
+				groups: ['admins']
+			},
+			joe_bob: {
+				password: 'test'
+			},
+			test: {
+				password: '121',
+				groups: ['limited']
+			}
+		}
+	}),
+	repos: {
+		'test-repo.git': {
+			access: {
+				joe_bob: githoist.access.READ + githoist.access.WRITE
+			},
+			owner: 'Kevin Smiles',
+			description: 'Describe it',
+			website: 'http://www.smiles.bak/',
+			title: 'This is a test Repo'
+		},
+		'frontier.git': {
+			owner: 'No One'
+		}
+	}
+})
